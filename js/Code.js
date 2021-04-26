9bd002002faab37867db65c6a20068a2a7d75b4e
Rehaan5823 <70743143+uefhuebb@users.noreply.github.com>
2021-04-26 19:34:58 +0530

    PRO-43

---
+++ js/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    text("C U T N I F O N", 700,150)
+    text("A T A D A S B A", 100,300)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    text("Hint: Stores all information !!", 100,320)
+    text("Hint: Performs a particular task !!", 700,170)
+
+// add code for displaying rest of the names and the hints.
+    
+
+}
